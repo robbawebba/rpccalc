5609d844f8e8a0278f4f860f05935527feebfb86
Robert Weber <robertweber95@gmail.com>
2019-08-08 22:59:43 -0700

    Remove unused main function from the prototyping phase

--- calc.go
+++ calc.go
@@ -1,33 +1,14 @@
 package rpccalc
 
-import (
-	"syscall"
-
-	"github.com/ethereum/go-ethereum/rpc"
-)
-
+// CalcService provides simple arithmetic operations such as Add and Subtract
 type CalcService struct{}
 
+// Add returns the result of adding a and b
 func (s *CalcService) Add(a, b int) int {
 	return a + b
 }
 
+// Subtract returns the result of subtracting b from a
 func (s *CalcService) Subtract(a, b int) int {
 	return a - b
-}
-
-func main() {
-	calculator := new(CalcService)
-	server := rpc.NewServer()
-	server.RegisterName("calculator", calculator)
-
-	//l, _ := net.ListenUnix("unix", &net.UnixAddr{Net: "unix", Name: "/tmp/calculator.sock"})
-	syscall.Mkfifo("/tmp/request", 0600)
-	syscall.Mkfifo("/tmp/response", 0600)
-	conn := NewFifoConn("/tmp/request", "/tmp/response")
-	for {
-		codec := rpc.NewJSONCodec(conn)
-		server.ServeCodec(codec, 0)
-	}
-
 }
